72962a1b4aa3bf4be44a16da9ebef7e0ad642dd0
Cenan Alhassan <cenan.alhassan@proton.me>
2024-06-13 13:40:20 +0300

    Barebones angular

--- DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f50dee217e4466839c13585ed07d5c53e590314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589c994031624f2e7305e83b8395066809e041ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
